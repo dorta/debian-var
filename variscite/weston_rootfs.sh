d02b279d290c46f9dd03cb281f6ab2eaba1bfb72
Diego Dorta <diego.d@variscite.com>
2023-08-27 13:39:33 -0300

    weston_rootfs: update scripts names for bluetooth and wifi
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- variscite/weston_rootfs.sh
+++ variscite/weston_rootfs.sh
@@ -155,9 +155,9 @@ function rootfs_install_var_bt {
 	# install variscite-bt service
 	install -m 0755 ${G_VARISCITE_PATH}/brcm_patchram_plus \
 		${ROOTFS_BASE}/usr/bin
-	install -d ${ROOTFS_BASE}/etc/bluetooth
-	install -m 0755 ${BRCM_UTILS_DIR}/${MACHINE}/variscite-bt \
-		${ROOTFS_BASE}/etc/bluetooth
+	install -d ${ROOTFS_BASE}/etc/bluetooth/variscite-bt.d
+	install -m 0755 ${BRCM_UTILS_DIR}/${MACHINE}/bcm43xx-bt \
+		${ROOTFS_BASE}/etc/bluetooth/variscite-bt.d
 	install -m 0644 ${BRCM_UTILS_DIR}/variscite-bt.service \
 		${ROOTFS_BASE}/lib/systemd/system
 	ln -sf /lib/systemd/system/variscite-bt.service \
@@ -166,9 +166,9 @@ function rootfs_install_var_bt {
 
 function rootfs_install_var_wifi() {
 	# install variscite-wifi service
-	install -d ${ROOTFS_BASE}/etc/wifi
-	install -m 0755 ${BRCM_UTILS_DIR}/${MACHINE}/variscite-wifi \
-		${ROOTFS_BASE}/etc/wifi
+	install -d ${ROOTFS_BASE}/etc/wifi/variscite-wifi.d
+	install -m 0755 ${BRCM_UTILS_DIR}/${MACHINE}/bcm43xx-wifi \
+		${ROOTFS_BASE}/etc/wifi/variscite-wifi.d
 	install -m 0644 ${BRCM_UTILS_DIR}/variscite-wifi.service \
 		${ROOTFS_BASE}/lib/systemd/system
 	ln -sf /lib/systemd/system/variscite-wifi.service \
